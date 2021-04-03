e5629bb50996adb98e0b219b8b9d2b7c6bb5ce7a
Yevhenii <y.stepanov1991@gmail.com>
2021-04-03 13:17:45 +0300

    update

--- Todoey/Controller/CategoryViewController.swift
+++ Todoey/Controller/CategoryViewController.swift
@@ -51,6 +51,7 @@ class CategoryViewController: SwipeTableViewController {
             
         }
         alert.addAction(action)
+        alert.addAction(UIAlertAction(title: "Dismiss", style: .cancel, handler: nil))
         present(alert, animated: true, completion: nil)
         
         //MARK: - Data Methods
